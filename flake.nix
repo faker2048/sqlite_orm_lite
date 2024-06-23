4e51a39b0cc4feebe56caefc4650d7d1edfe2d62
faker2048 <nspyia2002@gmail.com>
2024-06-24 00:14:02 +0800

    feat: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "sqlite-orm-lite is a C++ ORM library for SQLite3 database.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/24.05";
+
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: inputs.utils.lib.eachSystem [
+    "x86_64-linux" "i686-linux" "aarch64-linux" "x86_64-darwin"
+  ] (system: let
+    pkgs = import nixpkgs {
+      inherit system;
+
+      overlays = [];
+      
+      # Uncomment this if you need unfree software (e.g. cuda) for
+      # your project.
+      #
+      # config.allowUnfree = true;
+    };
+  in {
+    devShells.default = pkgs.mkShell rec {
+      # Update the name to something that suites your project.
+      name = "sqlite-orm-lite";
+
+      packages = with pkgs; [
+        # Development Tools
+        llvmPackages_19.clang
+        cmake
+        cmakeCurses
+
+        # Development time dependencies
+        gtest
+
+        # Build time and Run time dependencies
+        spdlog
+        abseil-cpp
+      ];
+
+      # Setting up the environment variables you need during
+      # development.
+      shellHook = let
+        icon = "f121";
+      in ''
+        export PS1="$(echo -e '\u${icon}') {\[$(tput sgr0)\]\[\033[38;5;228m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]} (${name}) \\$ \[$(tput sgr0)\]"
+      '';
+    };
+
+    packages.default = pkgs.callPackage ./default.nix {};
+  });
+}
